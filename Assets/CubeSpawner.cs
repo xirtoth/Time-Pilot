c3528bc5d3d20914bc692a943c99f91980f2ebd3
xirtoth <henry.heino11@gmail.com>
2019-05-04 14:36:13 +0300

    something stupid

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject spawnPoint;
+    private int maxCubes = 10;
+
+
+    private void Start()
+    {
+        for(int i = 0; i < maxCubes; i++)
+        {
+            GameObject go = Instantiate(cube, spawnPoint.transform.position, Quaternion.identity) as GameObject;
+            go.AddComponent<Rigidbody>();
+        }
+    }
+
+}
